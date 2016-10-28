9cb3edf3336bb689d80ba26ec27dbee87a327e17
Navi Kaur <nkaur901@gmail.com>
2016-10-28 15:13:03 -0400

    created function to show new list items

--- js/data.js
+++ js/data.js
@@ -1,20 +1,41 @@
 (function() {
 
+
+  function todo(items) {
+
+  var list = document.createElement('li');
+
+  // todo.innerText = document.querySelector('.new-todo').value;
+
+  var article = document.createElement('article');
+
+  var check = document.createElement('button');
+  check.classList.add('check');
+  article.appendChild(check);
+
+  var par = document.createElement('p');
+  par.innerText = items;
+  article.appendChild(par);
+
+  var del = document.createElement('button');
+  del.classList.add('delete');
+  del.innerText = 'X';
+  article.appendChild(del);
+
+  list.appendChild(article);
+
+  document.querySelector('ul').appendChild(list);
+
+}
+
 document.querySelector('form').addEventListener('submit', function handleEnter(eventobj){
   eventobj.preventDefault();
+  var todoList = document.querySelector('.new-todo').value;
 
-  // eventobj.target.childNodes[1].value;
-  console.log('hey');
+  todo(todoList);
 
-  console.log(eventobj);
 
-  var todo = document.createElement('li');
 
-  todo.innerText = document.querySelector('.new-todo').value;
-
-  todo.classList.add('new-item');
-  document.querySelector('ul').appendChild(todo);
-  console.log('addnewitem');
 
 })
 
