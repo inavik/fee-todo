d135aa554cd695ab32c8f17808d0324bb6cb2a89
Navi Kaur <nkaur901@gmail.com>
2016-12-29 18:16:19 -0500

    added more functionality to update incomplete number of tasks

--- js/ui.js
+++ js/ui.js
@@ -33,21 +33,31 @@
         var input = document.querySelector('.new-todo');
         todoApp(input.value);
         window.todo.todoData(input.value);
+
+        updateNumofTasksLeft();
     });
 
   document
-    .querySelector('.items')
+    .querySelector('ul.items')
     .addEventListener('click', function completed (eventObj) {
         if(eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'check') {
           if(eventObj.target.parentNode.className === 'completed') {
             eventObj.target.parentNode.classList.remove('completed');
+            window.todo.updateCounter(1);
           } else {
               eventObj.target.parentNode.classList.add('completed');
+              window.todo.updateCounter();
           }
+          updateNumofTasksLeft();
         } else if
         (eventObj.target.tagName === 'BUTTON' && eventObj.target.className === 'delete') {
           eventObj.target.parentNode.parentNode.remove();
+          window.todo.updateCounter();
         }
+        updateNumofTasksLeft();
     });
 
+  function updateNumofTasksLeft() {
+    document.querySelector('.incomplete-items').innerText = window.todo.totalCount();
+  }
 })();
