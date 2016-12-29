6680446a682827dbfa9499a3c244b3ba48e60091
Navi Kaur <nkaur901@gmail.com>
2016-12-29 18:13:35 -0500

    added an incomplete counter for number of tasks

--- js/data.js
+++ js/data.js
@@ -2,14 +2,14 @@
 (function() {
   window.todo = window.todo || {};
   window.todo.todoData = todoData;
-  window.todo.getTasksLeft = getTasksLeft;
+  window.todo.updateCounter = updateCounter;
+  window.todo.totalCount = totalCount;
 
   var numberOfTasksLeft = 0;
   var todoArray = [];
 
   function todoData(newTask) {
     numberOfTasksLeft++;
-
     var data = {
       text: newTask,
       time: Date.now()
@@ -17,4 +17,14 @@
     todoArray.push(data);
   }
 
+  function updateCounter(number) {
+    if(number === 1) {
+      numberOfTasksLeft++;
+    } else {
+      numberOfTasksLeft--;
+    }
+  }
+  function totalCount() {
+    return numberOfTasksLeft;
+  }
 })();
